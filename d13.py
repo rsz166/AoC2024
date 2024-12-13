2bd51765953edf65a948c14a8159154d9c9c612c
rs166 <szabolcs.roman6@gmail.com>
2024-12-13 09:29:05 +0100

    d13

--- d13.py
+++ d13.py
@@ -6,19 +6,19 @@ def getCost(machine):
     ay = machine[0][1]
     bx = machine[1][0]
     by = machine[1][1]
-    x = machine[2][0]
-    y = machine[2][1]
-    a = min(x // ax, 100)
-    b = min((x - a * ax) // bx, 100)
-    minCost = 1000 # 400 = 100 * (3 + 1) is the max number of button presses * token costs
-    while a >= 0:
-        if a * ax + b * bx == x and a * ay + b * by == y:
-            cost = a * 3 + b * 1
-            if cost < minCost:
-                minCost = cost
-        a -= 1
-        b = min((x - a * ax) // bx, 100)
-    return 0 if minCost == 1000 else minCost
+    x = machine[2][0] + 10000000000000
+    y = machine[2][1] + 10000000000000
+    
+    # a * ax + b * bx == x
+    # a * ay + b * by == y
+    c = ax / ay
+    b = round((x - c * y) / (bx - c * by))
+    a = round((x - b * bx) / ax)
+    cost = a * 3 + b * 1
+    # print(f'{a} {b} {cost}')
+    if a * ax + b * bx == x and a * ay + b * by == y:
+        return cost
+    return 0
 
 total = 0
 for machine in input:
